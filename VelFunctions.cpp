2119a7fd874c6092352d298e0071b14ea9189c41
Daniel Kanewske <danielkanewske@gmail.com>
2015-03-15 19:48:14 -0600

    Centralizeed Vel and Grad Vel calculations

---
+++ VelFunctions.cpp
@@ -0,0 +1,63 @@
+// Velocity and its partials called VelFunctions
+
+#include "VelFunctions.h"
+
+double HorizVel(double x,
+		double y)
+{
+  double z;
+  
+  z = -4.0 * y * (y - 1.0);// The max vel is 1 with a coeff of -4.
+  
+  return z;
+}
+
+double VertVel(double x,
+	      double y)
+{
+  double z;
+  
+  z = 0.0;
+  
+  return z;
+}
+
+double PartialHorizVelPartialx(double x,
+			      double y)
+{
+  double z;
+  
+  z = 0.0;
+  
+  return z;
+}
+
+double PartialHorizVelPartialy(double x,
+			      double y)
+{
+  double z;
+  
+  z = -8.0 * y + 4.0;
+  
+  return z;
+}
+
+double PartialVertVelPartialx(double x,
+			    double y)
+{
+  double z;
+  
+  z = 0.0;
+  
+  return z;
+}
+
+double PartialVertVelPartialy(double x,
+			    double y)
+{
+  double z;
+  
+  z = 0.0;
+  
+  return z;
+}
---
+++ VelFunctions.h
@@ -0,0 +1,22 @@
+#ifndef VelFunctions_H_
+#define VelFunctions_H_
+
+double HorizVel(double x,
+		double y);
+
+double VertVel(double x,
+	      double y);
+
+double PartialHorizVelPartialx(double x,
+			      double y);
+
+double PartialHorizVelPartialy(double x,
+			      double y);
+
+double PartialVertVelPartialx(double x,
+			    double y);
+
+double PartialVertVelPartialy(double x,
+			    double y);
+
+#endif
